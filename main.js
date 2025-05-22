d8a0837ad9c9823b6c8c51da61999c9a9edd9a41
pruje839 <jeffprumbaum@hotmail.com>
2025-05-22 18:29:32 +0200

    Fullscreen

--- main.js
+++ main.js
@@ -83,7 +83,12 @@ pulldown.onchange = function(evt) {
  }).addTo(map);
  controlElevation.load("data/etappe20.gpx");
 
+ //Minimap
+
 var gkTirol = new L.TileLayer("https://wmts.kartetirol.at/gdi_summer/{z}/{x}/{y}.png"); 
 var miniMap = new L.Control.MiniMap(gkTirol,{
     toggleDisplay: true
 }).addTo(map);
+
+//Fullscrean 
+map.addControl(new L.Control.Fullscreen()); 
